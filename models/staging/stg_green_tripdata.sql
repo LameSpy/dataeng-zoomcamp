372358432e802a4a7d1e222d742bb17722f50f4e
Дмитрий <LameSpy2008@yandex.ru>
2023-03-15 16:04:58 +0000

    fix stage

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -1,42 +1,43 @@
-/*with tripdata as 
+with tripdata as 
 (
   select *,
     row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
   from {{ source('staging','green_tripdata') }}
   where vendorid is not null 
-)*/
+)
 
 select
     -- identifiers
     {{dbt_utils.generate_surrogate_key(['vendorid', 'lpep_pickup_datetime'])}} as tripid,
-    cast(vendorid as integer) as vendorid,
-    cast(ratecodeid as integer) as ratecodeid,
-    cast(pulocationid as integer) as  pickup_locationid,
-    cast(dolocationid as integer) as dropoff_locationid,
+    cast(replace(vendorid, 'nan', '0') as numeric) as vendorid,
+    cast(replace(ratecodeid, 'nan', '0') as numeric) as ratecodeid,
+    cast(replace(pulocationid, 'nan', '0') as numeric) as  pickup_locationid,
+    cast(replace(dolocationid, 'nan', '0') as numeric) as dropoff_locationid,
     
     -- timestamps
-    cast(lpep_pickup_datetime as timestamp) as pickup_datetime,
-    cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime,
+    cast(replace(lpep_pickup_datetime, 'nan', '') as timestamp) as pickup_datetime,
+    cast(replace(lpep_dropoff_datetime, 'nan', '') as timestamp) as dropoff_datetime,
     
     -- trip info
-    store_and_fwd_flag,
-    cast(passenger_count as integer) as passenger_count,
-    cast(trip_distance as numeric) as trip_distance,
-    cast(trip_type as integer) as trip_type,
+    replace(store_and_fwd_flag, 'nan', '') as store_and_fwd_flag,
+    cast(replace(passenger_count, 'nan', '0') as numeric) as passenger_count,
+    cast(replace(trip_distance, 'nan', '0') as numeric) as trip_distance,
+    cast(replace(trip_type, 'nan', '0') as numeric) as trip_type,
     
     -- payment info
-    cast(fare_amount as numeric) as fare_amount,
-    cast(extra as numeric) as extra,
-    cast(mta_tax as numeric) as mta_tax,
-    cast(tip_amount as numeric) as tip_amount,
-    cast(tolls_amount as numeric) as tolls_amount,
-    cast(ehail_fee as numeric) as ehail_fee,
-    cast(improvement_surcharge as numeric) as improvement_surcharge,
-    cast(total_amount as numeric) as total_amount,
-    cast(payment_type as integer) as payment_type,
-    {{get_payment_type_description('payment_type')}} as payment_type_description,
-    cast(congestion_surcharge as numeric) as congestion_surcharge
-from {{ source('staging','green_tripdata') }}
+    cast(replace(fare_amount, 'nan', '0') as numeric) as fare_amount,
+    cast(replace(extra, 'nan', '0') as numeric) as extra,
+    cast(replace(mta_tax, 'nan', '0') as numeric) as mta_tax,
+    cast(replace(tip_amount, 'nan', '0') as numeric) as tip_amount,
+    cast(replace(tolls_amount, 'nan', '0') as numeric) as tolls_amount,
+    cast(replace(ehail_fee, 'nan', '0') as numeric) as ehail_fee,
+    cast(replace(improvement_surcharge, 'nan', '0') as numeric) as improvement_surcharge,
+    cast(replace(total_amount, 'nan', '0') as numeric) as total_amount,
+    cast(replace(payment_type, 'nan', '0') as numeric) as payment_type,
+    {{get_payment_type_description('cast(replace(payment_type, \'nan\', \'0\') as numeric)')}} as payment_type_description,
+    cast(replace(congestion_surcharge, 'nan', '0') as numeric) as congestion_surcharge
+from tripdata
+where rn = 1
 {%- if var('is_test_run', default=True) %}
 {# we can changa var in cli. Use this dbt run --select stg_green_tripdata --var 'is_test_run: false' #}
     limit 100
